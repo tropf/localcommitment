deca8a6f1cf572907fa77441c30fb97811309a72
Rolf Pfeffertal <foo@bar.com>
2017-12-04 17:40:00 +0100

    just checking if git is working properly...

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <sstream>
+#include <random>
+
+#define INCBIN_STYLE INCBIN_STYLE_SNAKE
+#define INCBIN_PREFIX
+#include "incbin.h"
+
+INCBIN(msgs, "commit_messages.txt");
 
 using namespace std;
-
-int main() {
-    cout << "Hello World!" << endl;
-}
 
 vector<string> get_text(string text) {
     vector<string> v;
     stringstream ss(text);
     string line;
     while(getline(ss, line)) {
-        v.push_back();
+        v.push_back(line);
     }
-    
     return v;
+}
+
+int main() {
+    string msgs(msgs_data, msgs_data + msgs_size);
+    auto v = get_text(msgs);
+
+    mt19937 rng;
+    rng.seed(random_device()());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(0, v.size() - 1);
+
+    cout << v[dist(rng)] << endl;
 }
