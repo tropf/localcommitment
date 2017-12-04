5c3eb1af4e788201aa8ba1dae4140275e5297c64
Franz Gloeckner <franzgloeckner@yahoo.de>
2017-12-04 18:10:59 +0100

    oops!

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,31 @@ INCBIN(msgs, "commit_messages.txt");
 
 using namespace std;
 
+const std::vector<string> NAMES = {"Nick", "Steve", "Andy", "Qi", "Fanny", "Sarah", "Cord", "Todd",
+    "Chris", "Pasha", "Gabe", "Tony", "Jason", "Randal", "Ali", "Kim",
+    "Rainer", "Guillaume", "Kelan", "David", "John", "Stephen", "Tom", "Steven",
+    "Jen", "Marcus", "Edy", "Rachel"}
+
+mt19937 get_random(int start, int end) {
+    mt19937 rng;
+    rng.seed(random_device()());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(start, end);
+    return rng;
+}
+
+
+
+
+void replaceAll(std::string& str, const std::string& from, const std::string& to) {
+    if(from.empty())
+        return;
+    size_t start_pos = 0;
+    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // In case "to" contains "from", like replacing "x" with "yx"
+    }
+}
+
 vector<string> get_text(string text) {
     vector<string> v;
     stringstream ss(text);
@@ -23,11 +48,14 @@ vector<string> get_text(string text) {
 
 int main() {
     string msgs(msgs_data, msgs_data + msgs_size);
+    mt19937 random1 = get_random(0, NAMES.size() - 1);
+    replaceAll(msgs, "XNAMEX", NAMES[random1]);
+    replaceAll(msgs, "XUPPERNAMEX", NAMES[random1]);
+
+
     auto v = get_text(msgs);
 
-    mt19937 rng;
-    rng.seed(random_device()());
-    std::uniform_int_distribution<std::mt19937::result_type> dist(0, v.size() - 1);
+    mt19937 rng = get_random(0, v.size() - 1);
 
     cout << v[dist(rng)] << endl;
 }
