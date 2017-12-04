61654b6df72445e718ac50c499b35d770ca16980
Franz Gloeckner <franzgloeckner@yahoo.de>
2017-12-04 17:32:45 +0100

    Version control is awful

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
 int main() {
     cout << "Hello World!" << endl;
+}
+
+vector<string> get_text(string text) {
+    vector<string> v;
+    stringstream ss(text);
+    string line;
+    while(getline(ss, line)) {
+        v.push_back();
+    }
+    
+    return v;
 }
